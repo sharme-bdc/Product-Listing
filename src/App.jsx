cf0d64c01e3bf71722b3408cb68d2a9c2be3faaf
sharme-bdc <sharmeakter777@gmail.com>
2025-06-03 06:31:15 -0700

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <>
-      <p>dsfdfs</p>
+      <header className="bg-[#1E1E1E]">
+        <div ClassName="container max-w-7xl max-auto">
+          <div className="flex">
+            <img src="/src/assets/logo.png" className="w-auto h-auto"></img>
+          </div>
+        </div>
+      </header>
     </>
   )
 }
