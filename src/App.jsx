f2985a6b5aaba0a06bb7eebac7d4d546a3bb0746
sharme-bdc <sharmeakter777@gmail.com>
2025-06-05 08:24:20 -0700

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 function App() {
   return (
-    <>
-      <header className="bg-[#1E1E1E]">
-        <div ClassName="container max-w-7xl max-auto">
-          <div className="flex">
-            <img src="/src/assets/logo.png" className="w-auto h-auto"></img>
-          </div>
+    <header className="bg-[#1E1E1E] py-4">
+      <div ClassName="container">
+        <div className="flex flex-col sm:flex-row justify-between items-center mx-[40px] ">
+          <img src="/src/assets/logo.png" className="w-[170px] h-[60px]"></img>
+          <nav className="flex list-none gap-4 text-white justify-center items-center">
+            <li className="relative">
+              <a href="#">Products</a>
+            </li>
+            <li><a href="#">Track Order</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">About</a></li>
+          </nav>
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   )
 }
 
